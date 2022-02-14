44eb5a2fba728fc19a9656fb6abbcfcb8fd42f9c
AtharvaThorve <aaathorve@gmail.com>
2022-02-14 22:31:46 +0530

    Added support for ipfs

--- CredentialSchema.sol
+++ CredentialSchema.sol
@@ -6,18 +6,10 @@ pragma experimental ABIEncoderV2;
 import "./DID.sol";
 
 contract CredentialSchema is DIDContract {
-    struct Properties {
-        string key;
-        string propType;
-        string propFormat;
-    }
-
     struct CredSchema {
         string[] context;
         string id;
-        string title;
-        string description;
-        Properties[] properties;
+        string ipfsHash;
     }
 
     event CreateCredSchema(string did);
@@ -25,25 +17,35 @@ contract CredentialSchema is DIDContract {
 
     mapping(string => CredSchema) public credSchemaStore;
 
-    string[] schemaContext = ["https://w3id.org/did/v1", "https://w3id.org/security/v2", "https://w3c-ccg.github.io/vc-json-schemas/context/vc-json-schema-v0.0.jsonld"];
+    string[] schemaContext = [
+        "https://w3id.org/did/v1",
+        "https://w3id.org/security/v2",
+        "https://w3c-ccg.github.io/vc-json-schemas/context/vc-json-schema-v0.0.jsonld"
+    ];
 
-    function getCredSchema(string memory did) public returns(CredSchema memory) {
-        require(bytes(credSchemaStore[did].id).length != 0, "DID does not exist");
+    function getCredSchema(string memory did)
+        public
+        returns (CredSchema memory)
+    {
+        require(
+            bytes(credSchemaStore[did].id).length != 0,
+            "DID does not exist"
+        );
         emit GetCredSchema(credSchemaStore[did]);
         return credSchemaStore[did];
     }
 
-    function createCredSchema(string memory issuerDid, string memory hash, string memory title, string memory description, Properties[] memory properties) public checkDidExists(issuerDid) returns(string memory) {
+    function createCredSchema(
+        string memory issuerDid,
+        string memory hash,
+        string memory ipfsHash
+    ) public checkDidExists(issuerDid) returns (string memory) {
         bytes memory b;
         b = abi.encodePacked("did:schm:");
         b = abi.encodePacked(b, hash);
         string memory did = string(b);
         credSchemaStore[did].id = did;
-        for(uint i = 0; i < properties.length; i++) {
-            credSchemaStore[did].properties.push(properties[i]);
-        }
-        credSchemaStore[did].description = description;
-        credSchemaStore[did].title = title;
+        credSchemaStore[did].ipfsHash = ipfsHash;
         credSchemaStore[did].context = schemaContext;
         emit CreateCredSchema(did);
         return did;
